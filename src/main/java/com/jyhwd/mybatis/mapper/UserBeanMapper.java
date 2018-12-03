9a6943ed4faa219b6b748c37857ee669b3d3ff21
tanjiuyang <jiuyang_tan@163.com>
2018-12-03 21:33:46 +0800

    mybatisDemo

---
+++ src/main/java/com/jyhwd/mybatis/mapper/UserBeanMapper.java
@@ -0,0 +1,18 @@
+package com.jyhwd.mybatis.mapper;
+
+import com.jyhwd.mybatis.beans.UserBean;
+
+/**
+ * @创建者：tanjiuyang
+ * @时间：16:17 2018/12/3
+ */
+public interface UserBeanMapper {
+    /**
+     * 新增用户
+     * @author tanjiuyang
+     * @date 2018/12/3 16:18
+     * @param user
+     * @return int
+     */
+    public int insertUser(UserBean user);
+}
